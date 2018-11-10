d3e12f69712bb4db3900bc0e1e1eb985b0eb2048
angelazhou123 <angelazhou123@gmail.com>
2018-11-10 15:12:17 -0500

    Update image_upload.js

--- image_upload.js
+++ image_upload.js
@@ -1,3 +1,18 @@
+const app = new Clarifai.App({
+    apiKey: 'YOUR_API_KEY'
+});
+
+// predict the contents of an image by passing in a url
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+    function(response) {
+        console.log(response);
+    },
+    function(err) {
+        console.error(err);
+    }
+);
+
+
 function readURL(input) {
     if (input.files && input.files[0]) {
 
